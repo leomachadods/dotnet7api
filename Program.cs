0aba539855f5b245c3ef05005ddbaf1cd7d058c4
leomachadods <leomachadods@gmail.com>
2023-04-07 21:52:05 -0300

    Added: Dependency injection, Dto model, Auto mapper, Response service and asynchronous methods.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+global using LikeApi.Models;
+global using LikeApi.Services.CharacterService;
+global using LikeApi.Dtos.Character;
+global using AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
